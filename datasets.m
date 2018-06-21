6593a8cd4330577e84b786fdeb3fe7b671533d4e
Sam Hofman <hofmansam@hotmail.com>
2018-06-21 11:14:44 +0200

    Een beetje zitten prutsen

--- datasets.m
+++ datasets.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = datasets(inputArg1,inputArg2)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+filename = 'Flights.xlsx';
+T1 = readtable(filename,'Range','A:B');
+T2 = readtable(filename,'Range','D:I');
+T = [T1 T2];
+variables = T.Properties.VariableNames;
+
+%% Check if 29 July is in flight interval
+% for i = 1:height(T)   %Iterate over all rows
+%     tlower = T(i, 3); %3rd column is lower time bound
+%     tupper = T(i,4);  %4th column is upper time bound
+%     t = '29/05/2005';
+%     tf = isbetween(t,tlower,tupper)
+% end
 
