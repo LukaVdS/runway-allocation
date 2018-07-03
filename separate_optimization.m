2e7a11e0e32a7fe59317251c783128b36d57d5e8
luckiluka@gmail.com <luckiluka@gmail.com>
2018-07-03 17:13:18 +0200

    adding christophes part (constraint 2)

--- separate_optimization.m
+++ separate_optimization.m
@@ -13,6 +13,13 @@ tablaux = 'Tables.xlsx';
 testset = xlsread(tablaux,'testset','P2:P141');
 time = xlsread(tablaux,'flights','E2:E11');
 
+%% CHRIS
+tablaux_chris = 'Tables_christophe.xlsx';
+flights_chris = xlsread(tablaux_chris, 'flights', 'A1:D11');
+t_to_RWY_chris = xlsread(tablaux_chris, 't_to_RWY', 'A1:C6');
+RWY_dependability = xlsread(tablaux_chris, 'RWY_DEPENDABILITY', 'A1:P9');
+%% Chris END
+
 %% Cost Calculations
 %% 
 %%  Assumptions
@@ -102,7 +109,40 @@ end
 %         cplex_fuel.addRows(0, C3, 1, sprintf('Runway_Occupation_%d_%d',f,d));
 %     end
 % end
+%% Chris
+t_RO_min = min(flights_chris(:, 4)) + min(delay) + min(t_to_RWY_chris); % Calculates earliest possible arrival
+t_RO_max = max(flights_chris(:, 4)) + max(delay) + max(t_to_RWY_chris); % Calculates latest possible arrival
 
+for t_c = t_RO_min:20:t_RO_max % Between the possible arrivals check every 20 seconds
+    for r_c = 1:R              % Check for both runways
+        C2 = zeros(1, DV_fuel);% Create new constraint, analyze every DV (f,r,d)
+        for f = 1:F % for each flight
+            for r = 1:R % to each runway                    
+                for d = delay % with each delay
+                    % find t_at_RWY for f,r,d
+                    temp = t_to_RWY_chris; % temp will become the time to runway for the IAF and runway
+                    temp = temp(temp(:,1)==r,2:end); % Select correct Arrival Runway (=r)             
+                    temp = temp(temp(:,1)==flights_chris(f,2),2); % Select correct IAF (=flights_chris(f,2)
+                    t_at_RWY_f_r_d = flights_chris(f,4) + temp + d; % Time at runway = time IAF + time to runway + delay
+                    % find times around t_at_RWY occupied based on r, r_c,
+                    % type
+                    temp = RWY_dependability; % temp will become the dependability row for the arrival RWY, dependent RWY, and type
+                    temp = temp(temp(:,1)==r,2:end); % Select correct Arrival RWY (=r)
+                    temp = temp(temp(:,1)==r_c,2:end); % Select correct Dependent RWY (=r_c)
+                    temp = temp(temp(:,1)==flights_chris(f,3),2:end); % Select correct Weight Class (=flights_chris(f,3))
+                    t_around_all = RWY_dependability(1,4:end); % These are all possible blocking ranges around the arrival time (=-120:20:120)
+                    t_around = t_around_all(temp==1); % Select blocking range corresonding to dependability row (=temp)
+                    t_range = t_around + t_at_RWY_f_r_d; % The time occupying the runway is the time of arrival around the range where it influences the runway
+                    if ismember(t_c, t_range) % if the flight (f) landing at runway (r) with delay (d) influences the depending runway (r_c) at that time (t_c)
+                        C2(Xindex(f,r,d)) = 1; % set its block to one.
+                    end
+                end
+            end
+        end
+        %cplex_fuel.addRows(0, C2, 1, sprintf('Runway_Occupation_RW_%d_t_%d',r_c,t_c))
+    end
+end
+%% Chris END
 
 
 %% NOISE CPLEX
